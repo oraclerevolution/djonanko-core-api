e3df1dbabade98d6b896337c6159503a95c7a9b5
oraclerevolution <mac.n.drix@gmail.com>
2023-11-29 10:36:37 +0000

    first commit with database config

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { HelperModule } from './helper/helper.module';
+
+@Module({
+  imports: [HelperModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
