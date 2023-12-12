cb3e509496c522cd081cc79585620338b33cedd2
oraclerevolution <mac.n.drix@gmail.com>
2023-12-12 13:17:12 +0000

    prise en compte du mode d'abonnement pour caculer les fees

--- src/transfert/transfert.service.ts
+++ src/transfert/transfert.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Transfert } from './entities/transfert.entity';
 import { Repository } from 'typeorm';
@@ -15,7 +15,7 @@ export class TransfertService {
         @InjectRepository(Transfert) private readonly repository: Repository<Transfert>,
         private readonly userService: UserService,
         private readonly historiqueService: HistoriquesService
-    ) {}
+    ) { }
 
     /**
      * Makes a transfer with the provided payload.
@@ -24,38 +24,62 @@ export class TransfertService {
      * receiverPhoneNumber, and amount.
      * @return {Promise<any>} - A promise that resolves to the result of the transfer.
      */
-    async makeTransfert(payload: MakeTransfertDto): Promise<any> {
-        const {senderPhoneNumber, receiverPhoneNumber, amount} = payload
+    async makeTransfert(payload: MakeTransfertDto): Promise<Transfert> {
+        const { senderPhoneNumber, receiverPhoneNumber, amount } = payload
         const getSenderInfos = await this.userService.getUserByPhoneNumber(senderPhoneNumber)
         const getReceiverInfos = await this.userService.getUserByPhoneNumber(receiverPhoneNumber)
-        const balanceAfterSending = parseInt(getSenderInfos.solde) - parseInt(amount)
-        const transfert = new Transfert()
-        transfert.amount = amount
-        transfert.amountBeforeSending = getSenderInfos.solde
-        transfert.amountAfterSending = (balanceAfterSending).toString()
-        transfert.senderPhoneNumber = senderPhoneNumber
-        transfert.receiverPhoneNumber = receiverPhoneNumber
-
-        const updateSenderBalance = await this.userService.updateUser(getSenderInfos.id, {
-            solde: balanceAfterSending.toString()
-        })
-
-        // const updateReceiverBalance = await this.userService.updateUser(getReceiverInfos.id, {
-        //     solde: (parseInt(getReceiverInfos.solde) + parseInt(amount)).toString()
-        // })
-
-        if(updateSenderBalance.affected !== 0) { 
-            const createTransfert = await this.repository.save(transfert)
-            if(createTransfert) {
-                await this.historiqueService.createHistorique({
-                    sender: getSenderInfos,
-                    receiver: getReceiverInfos,
-                    transactionType: TransactionType.TRANSFERT,
-                    amount: amount,
-                    icon:'send'
-                })
-                return createTransfert
+        if(getReceiverInfos){
+            const balanceAfterSending = parseInt(getSenderInfos.solde) - this.getTransactionFees(parseInt(amount), getSenderInfos.premium)
+            const transfert = new Transfert()
+            transfert.amount = amount
+            if (getSenderInfos.premium === true) {
+                transfert.fees = "0"
+            } else {
+                transfert.fees = (0.01 * parseInt(amount)).toString()
             }
+            transfert.amountBeforeSending = getSenderInfos.solde
+            transfert.amountAfterSending = (balanceAfterSending).toString()
+            transfert.senderPhoneNumber = senderPhoneNumber
+            transfert.receiverPhoneNumber = receiverPhoneNumber
+    
+            const updateSenderBalance = await this.userService.updateUser(getSenderInfos.id, {
+                solde: balanceAfterSending.toString()
+            })
+    
+            const updateReceiverBalance = await this.userService.updateUser(getReceiverInfos.id, {
+                solde: (parseInt(getReceiverInfos.solde) + parseInt(amount)).toString()
+            })
+    
+            if (updateSenderBalance.affected === 1 && updateReceiverBalance.affected === 1) {
+                const createTransfert = await this.repository.save(transfert)
+                if (createTransfert) {
+                    await this.historiqueService.createHistorique({
+                        sender: getSenderInfos,
+                        receiver: getReceiverInfos,
+                        transactionType: TransactionType.TRANSFERT,
+                        amount: amount,
+                        icon: 'send'
+                    })
+                    return createTransfert
+                }
+            }
+        }else{
+            throw new NotFoundException("Le destinataire n'as pas de compte djonanko")
+        }
+    }
+
+    /**
+ * Calculates the transaction fees for a given amount.
+ *
+ * @param {number} amount - The amount for which to calculate the transaction fees.
+ * @return {number} - The total amount including the transaction fees.
+ */
+    getTransactionFees(amount: number, mode: boolean): number {
+        if (mode === true) {
+            return amount;
+        } else {
+            const fees = 0.01 * amount;
+            return amount + fees;
         }
     }
 }
