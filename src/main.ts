a0bd1115afadcad4cc9914b817ca7c197cdb35e9
oraclerevolution <mac.n.drix@gmail.com>
2023-12-31 00:39:42 +0000

    open api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('DJONANKO CORE API')
+    .setDescription('Djonanko core API description')
+    .setVersion('1.0')
+    .addTag('djonanko')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(3002);
 }
 bootstrap();
