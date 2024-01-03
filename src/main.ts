5638148e7ca402d25edd13c007842a8054deeceb
oraclerevolution <mac.n.drix@gmail.com>
2024-01-03 18:48:07 +0000

    use port API in .env

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as helmet from "helmet";
+import * as cookieParser from "cookie-parser"
 
+const cors = require('cors')
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors())
 
   const config = new DocumentBuilder()
     .setTitle('DJONANKO CORE API')
     .setDescription('Djonanko core API description')
     .setVersion('1.0')
-    .addTag('djonanko')
+    .addTag('Djonanko')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('documentation', app, document);
 
-  await app.listen(3002);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>("API_PORT");
+  app.use(cookieParser())
+
+  await app.listen(port);
 }
 bootstrap();
