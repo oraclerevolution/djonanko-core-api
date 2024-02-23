6d6d80ad0e2709eb36a1af3bcd184a2ecad20c15
oraclerevolution <mac.n.drix@gmail.com>
2024-02-23 15:08:16 +0000

    openapi documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('DJONANKO CORE API')
+    .setDescription('The Core API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3002);
 }
 bootstrap();
